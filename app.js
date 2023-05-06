a6ac763d9e9232fb9484ee25800b812e3a5ce9e6
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-05-06 23:31:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,28 +3,20 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
-    initTabs();
+    window.addEventListener("hashchange", showTab);
+    showTab();
 }
 
-function initTabs() {
-    document.querySelectorAll(".tab-link").forEach(addClick); // add click event to all tab-links
-    // if hash - if tab already selected (see #hash in browser)
+function showTab() {
+    let tab = "#home";
     if (location.hash) {
-        showTab(location.hash);
-    } else {
-        showTab("#home");
+        tab = location.hash;
     }
-}
 
-function addClick(tabLink) {
-    tabLink.addEventListener("click", () => showTab(tabLink.getAttribute("href")));
-}
-
-function showTab(selector) {
-    const tabLink = document.querySelector(`a.tab-link[href="${selector}"]`); // reference to link
+    const tabLink = document.querySelector(`a.tab-link[href="${tab}"]`); // reference to link
     hideAllTabs(); // hide all tabs
     tabLink.classList.add("active"); // add .active to "this" link
-    document.querySelector(selector).classList.add("active"); // add .active to the tab you want to show
+    document.querySelector(tab).classList.add("active"); // add .active to the tab you want to show
 }
 
 function hideAllTabs() {
